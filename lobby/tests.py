8690c58e960a35bf3129fd5dc8f5ef39179eb99e
David-DEV <david.ezequielima2005@gmail.com>
2026-06-27 14:12:41 -0300

    ci: implementação de pipeline de testes e GitHub Actions
    Adição da esteira de Integração Contínua (CI) via GitHub Actions (.github/workflows) configurada com banco PostgreSQL temporário e Python 3.14.3. Criação da suíte de testes unitários em lobby/tests.py para validar regras de negócio (bloqueio de duplicidade) e segurança (proteção de exclusão). Atualização do arquivo requirements.txt com o mapeamento completo das dependências do ambiente.

--- lobby/tests.py
+++ lobby/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from lobby.models import Avaliacao, Jogo
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class JogoTestCase(TestCase):
+    def test_nao_duplicar_jogo(self):
+        # Cria o jogo
+        Jogo.objects.create(titulo="Hollow Knight", genero="Ação e Aventura", plataforma="PC") # <-- Mude aqui
+        
+        # Tenta cadastrar de novo via POST
+        response = self.client.post(reverse('add_game'), {
+            'titulo': 'Hollow Knight',
+            'genero': 'Ação e Aventura', # <-- Mude aqui para ser igual ao de cima
+            'plataforma': 'PS5'
+        })
+        
+        # Verifica se continua com apenas 1
+        self.assertEqual(Jogo.objects.count(), 1)
+
+class AvaliacaoTestCase(TestCase):
+    def setUp(self):
+        # O método setUp roda antes de cada teste para preparar o "terreno"
+        self.usuario_autor = User.objects.create_user(username='autor', password='123')
+        self.usuario_invasor = User.objects.create_user(username='invasor', password='123')
+        
+        self.jogo = Jogo.objects.create(titulo="Resident Evil 5", genero="Ação", plataforma="PC")
+        
+        self.avaliacao = Avaliacao.objects.create(
+            jogo=self.jogo,
+            usuario=self.usuario_autor,
+            nota=9.0,
+            comentario="Wesker."
+        )
+
+    def test_invasor_nao_pode_apagar_avaliacao(self):
+        # Fazemos login com o usuário invasor
+        self.client.login(username='invasor', password='123')
+        
+        # Ele manda o POST para deletar a review do outro usuário
+        response = self.client.post(reverse('deletar_avaliacao', args=[self.avaliacao.id]))
+        
+        #C Verifica se o invasor não conseguiu apagar a review
+        self.assertEqual(Avaliacao.objects.count(), 1)
+
+    def test_autor_pode_apagar_avaliacao(self):
+        #Fazemos login com o verdadeiro dono da review
+        self.client.login(username='autor', password='123')
+        
+        # Ele manda o POST para deletar a própria review
+        response = self.client.post(reverse('deletar_avaliacao', args=[self.avaliacao.id]))
+        
+        # Verificamos se apagou com sucesso
+        self.assertEqual(Avaliacao.objects.count(), 0)
