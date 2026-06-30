32609d80647d8733a626dbd836ec1e644fe92f07
Enderson Carvalho <enderson.s.carvalho@hotmail.com>
2026-06-30 02:34:43 -0300

    Preparando para deploy
    Organização pra quando for dar deploy do sistema no EC2

--- core/settings.py
+++ core/settings.py
@@ -29,9 +29,9 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
